5ccbac47da99d530f4b3b5429df97d1c59d89d9e
Mayank sharma <personal@Mayanks-MacBook-Pro.local>
2023-11-21 10:36:06 +0530

    setup clerkjs for auth

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { clerkPlugin } from 'vue-clerk/plugin'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App)
+app.use(clerkPlugin, {
+    publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+})
+
+app.mount('#app')
